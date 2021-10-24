8cea42069f64eb2aeed45242ebc0e51e3acc5cba
manojmac07 <93003636+manojmac07@users.noreply.github.com>
2021-10-24 20:28:18 +0530

    Find Distance and generate_series

--- DDL_LOAD.sql
+++ DDL_LOAD.sql
@@ -79,6 +79,50 @@ SELECT aws_s3.table_import_from_s3(
    '',
    '')
 
+/* 
+* Task 1 QUeries
+*/
+
+--Distance from current place or bangalore
+select c.*, SQRT(POW(69.1 * (12::float -  c.lat ::float), 2) + 
+    POW(69.1 * (79::float - c.lng ::float) * COS(12::float / 57.3), 2)
+) as distance from public.city as c
+
+select * from (
+SELECT  *,( 3959 * acos( cos( radians(6.414478) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(12.466646) ) + sin( radians(6.414478) ) * sin( radians( lat ) ) ) ) AS distance 
+FROM public.city
+) al
+where distance <2000
+ORDER BY distance
+LIMIT 20;
+
+
+-- All users 
+
+with city_table as (
+select * from (
+SELECT  *,( 3959 * acos( cos( radians(6.414478) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(12.466646) ) + sin( radians(6.414478) ) * sin( radians( lat ) ) ) ) AS distance 
+FROM public.city
+)a1),
+user_table as(
+select * from (
+SELECT  *,( 3959 * acos( cos( radians(6.414478) ) * cos( radians( ud.latitude ) ) * cos( radians( ud.longitude ) - radians(12.466646) ) + sin( radians(6.414478) ) * sin( radians( ud.latitude ) ) ) ) AS distance 
+FROM public.user_details ud 
+) b1)
+select ct.*,ut.* from city_table ct join user_table ut on  ct.distance::int = ut.distance::int;
+
+
+/*
+* Task 2 to build the histogram
+*/
+-- To build the range from 0 to 35
+SELECT (five*5)::text||'-'||(five*5+5)::text AS Distance,
+           five*5 AS r_min, five*5+5 AS r_max
+      FROM generate_series(0,6) AS t(five)
+      
+    
+
+
   
   
    
